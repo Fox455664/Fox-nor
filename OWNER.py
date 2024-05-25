dcb4c9a38ef21e3e08ca1a205803c35810f36226
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2024-05-25 15:09:34 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,10 +1,10 @@
 # متطلبات التنصيب #
 OWNER = ["F_o_x_5"]
-OWNER_NAME = " Royal"
+OWNER_NAME = "F_o_x_5"
 BOT_TOKEN = "7002439220:AAGEKskizmJ9W9l83hiaSuf6LfxmWV3oLio"
 DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/U0_Ux"
 GROUP = "https://t.me/U0_UG"
 VIDEO = "https://telegra.ph/file/770e2d5df0b50264097b2.jpg"
-LOGS = "Royal"
+LOGS = "F_o_x_5"
 
