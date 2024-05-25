25a501f28a62877ac1b168638695f4a1952bf035
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2024-05-25 14:17:42 +0300

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["Ux_Uv"]
+OWNER_NAME = " Royal"
+BOT_TOKEN = "6022370529:AAHM5anvuHDDhKN52ypmcoKTLWnWFEPVDDE"
+DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/U0_Ux"
+GROUP = "https://t.me/U0_UG"
+VIDEO = "https://telegra.ph/file/770e2d5df0b50264097b2.jpg"
+LOGS = "Royal"
+
