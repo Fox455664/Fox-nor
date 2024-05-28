43f2c8ee1acc9845ee22e5e878fd228e05b19d25
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2024-05-28 22:38:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,18 +2,25 @@ from pyrogram import Client, idle
 from config import API_ID, API_HASH, BOT_TOKEN
 from pyromod import listen
 
+# إنشاء عميل بوت
 bot = Client(
-    "mo",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins=dict(root="Maker")
+    "mo",  # اسم الجلسة
+    api_id=API_ID,  # معرف API
+    api_hash=API_HASH,  # هاش API
+    bot_token=BOT_TOKEN,  # رمز البوت
+    plugins=dict(root="Maker")  # المسار إلى الإضافات
 )
 
+# تعريف وظيفة بدء البوت
 async def start_bot():
-    print("[INFO]: STARTING BOT CLIENT")
-    await bot.start()
+    print("[INFO]: STARTING BOT CLIENT")  # طباعة رسالة بدء البوت
+    await bot.start()  # بدء البوت
     # استبدل '@F_o_x_5' بمعرف الدردشة الخاص بك
-    await bot.send_message('@F_o_x_5', "**تم تشغيل الصانع عزيزي المطور ،**")
-    print("[INFO]: تم تشغيل الصانع وارسال رسالة للمطور🔮.")
-    await idle()
+    await bot.send_message('@F_o_x_5', "**تم تشغيل الصانع عزيزي المطور،**")
+    print("[INFO]: تم تشغيل الصانع وإرسال رسالة للمطور🔮.")  # طباعة رسالة تأكيد
+    await idle()  # الانتظار للأحداث
+
+# بعد تحديث الكود، إذا استمرت في تلقي هذا الخطأ:
+# 2024-05-28T19:19:08.611152+00:00 app[worker.2]:     raise BadMsgNotification(result.error_code)
+# 2024-05-28T19:19:08.611167+00:00 app[worker.2]: pyrogram.errors.BadMsgNotification: [16] The msg_id is too low, the client time has to be synchronized.
+# 2024-05-28T19:19:08.769354+00:00 heroku[worker.2]: State changed from up to crashed
