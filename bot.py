0ed227a192a8aee37b18cef0d9666100fd844b04
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2024-05-28 22:09:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,13 +8,12 @@ bot = Client(
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
     plugins=dict(root="Maker")
-    )
+)
 
 async def start_bot():
     print("[INFO]: STARTING BOT CLIENT")
     await bot.start()
-    royal = "Ux_Uv"
-    # تأكد من استبدال 'chat_id' بمعرف الدردشة الصحيح
-    await bot.send_message(chat_id=royal, text="**تم تشغيل الصانع عزيزي المطور ،**")
+    # استبدل '@F_o_x_5' بمعرف الدردشة الخاص بك
+    await bot.send_message('@F_o_x_5', "**تم تشغيل الصانع عزيزي المطور ،**")
     print("[INFO]: تم تشغيل الصانع وارسال رسالة للمطور🔮.")
     await idle()
