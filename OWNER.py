8b3dd95b772683522d9d47666ab5fa131ac4194b
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2024-05-29 09:55:28 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,7 +1,7 @@
 # متطلبات التنصيب #
 OWNER = ["F_o_x_5"]
 OWNER_NAME = "F_o_x_5"
-BOT_TOKEN = "7107627916:AAFRd85PmDe8QB5jraXSFdxsaXtca88JCq4"
+BOT_TOKEN = "7061133762:AAHFfyaLbgkRFQ7LuU4MOWBKhHv9kkFfrjE"
 DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/U0_Ux"
 GROUP = "https://t.me/U0_UG"
