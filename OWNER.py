2bc24f8faf201aba8079975654233af9f7ad9d79
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2024-06-06 01:25:41 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,10 +1,10 @@
 # متطلبات التنصيب #
-OWNER = ["F_o_x_5"]
-OWNER_NAME = "F_o_x_5"
-BOT_TOKEN = "7061133762:AAHFfyaLbgkRFQ7LuU4MOWBKhHv9kkFfrjE"
+OWNER = ["foxmuisic"]
+OWNER_NAME = "foxmuisic"
+BOT_TOKEN = "6847235687:AAFxduJMr1uqXGRQx-e1WUTZZQR20QEabnE"
 DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
-CHANNEL = "https://t.me/U0_Ux"
-GROUP = "https://t.me/U0_UG"
+CHANNEL = "https://t.me/gfffffftfff"
+GROUP = "https://t.me/fox46788"
 VIDEO = "https://telegra.ph/file/770e2d5df0b50264097b2.jpg"
-LOGS = "F_o_x_5"
+LOGS = "foxmuisic"
 
